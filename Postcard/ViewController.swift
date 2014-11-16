903713dce5b75fcbef71b03778f2eef5e85a4e0c
Axel Zuziak <zuziakaxel@MacBook-Air.local>
2014-11-16 22:50:04 +0100

    new label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -29,6 +30,14 @@ class ViewController: UIViewController {
 
     @IBAction func sendMainButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        
+        
         messageLabel.text = "Wysłano do: \(enterNameTextField.text)"
         enterNameTextField.text = ""
         enterMessageTextField.text = ""
